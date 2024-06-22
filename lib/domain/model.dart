f1ba9b387ab9a64ed3b3d66221d779eb41b65854
youssefhatem03 <youhatem2000@gmail.com>
2024-06-22 21:12:56 +0300

    models and extenstions

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -8,3 +8,26 @@ class SliderObject {
 }
 
 
+class Customer{
+  String id;
+  String name;
+  int numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts{
+  String phone;
+  String link;
+  int email;
+
+  Contacts(this.phone, this.link, this.email);
+}
+
+
+class Authentication{
+  String customer;
+  String contacts;
+
+  Authentication(this.customer, this.contacts);
+}
